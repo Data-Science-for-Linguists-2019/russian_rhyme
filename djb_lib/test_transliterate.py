7c3d352e9cafce13e32c2952925052dfbe1ace47
David J Birnbaum <djbpitt@gmail.com>
2019-02-11 16:27:24 -0500

    first commit of nose tests

--- djb_lib/test_transliterate.py
+++ djb_lib/test_transliterate.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
-
+from djb_lib import djb_library
 
 class TestTransliterate(TestCase):
-    def test_transliterate(self):
-        self.fail()
+    def test_transliterate_1(self):
+        expected = "hi"
+        self.assertEqual(djb_library.transliterate("hi"), expected)
+    def test_transliterate_2(self):
+        expected = ""
+        self.assertEqual(djb_library.transliterate(""), expected)
