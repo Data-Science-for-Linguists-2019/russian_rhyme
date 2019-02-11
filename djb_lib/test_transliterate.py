ffb739116f1d0078d20c3b7c4ec5337c5e3017bd
David J Birnbaum <djbpitt@gmail.com>
2019-02-11 18:27:43 -0500

    add tests for flatten, enable coverage

--- djb_lib/test_transliterate.py
+++ djb_lib/test_transliterate.py
@@ -8,3 +8,20 @@ class TestTransliterate(TestCase):
     def test_transliterate_2(self):
         expected = ""
         self.assertEqual(djb_library.transliterate(""), expected)
+
+class TextFlatten(TestCase):
+    def test_flatten_stress(self):
+        expected = "бЕрег"
+        self.assertEqual(djb_library._flatten("<line>б<stress>е</stress>рег</line>"), expected)
+    def test_flatten_lc(self):
+        expected = "берег"
+        self.assertEqual(djb_library._flatten("<line>Берег</line>"), expected)
+    def test_flatten_punc_hyphen(self):
+        expected = "кто-то"
+        self.assertEqual(djb_library._flatten("<line>кто-то</line>"), expected)
+    def test_flatten_punc_period(self):
+        expected = "берег"
+        self.assertEqual(djb_library._flatten("<line>берег.</line>"), expected)
+    def test_flatten_punc_question(self):
+        expected = "берег"
+        self.assertEqual(djb_library._flatten("<line>берег?</line>"), expected)
