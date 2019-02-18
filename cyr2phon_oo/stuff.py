8599585bb8ea947026e60bdacfa199a564e43b0b
David J Birnbaum <djbpitt@gmail.com>
2019-02-18 01:00:10 -0500

    fine tuning of function factory

--- cyr2phon_oo/stuff.py
+++ cyr2phon_oo/stuff.py
@@ -1,14 +1,39 @@
 import regex as re
 from typing import Optional
 
+
 # experiment with function factory to bind constants with function
 
-class f:
-    def __init__(self, name: str, constants: Optional[dict] = {}, callbacks: Optional[dict] = {}, function:str = ""):
+class F:
+    """
+    Create named function, bundling constants and callback with name and function code
+
+    Keyword parameters:
+    name (str; required): function name
+    constants (dict; optional): name:value; for regex expressions and translation tables
+    callbacks (dict; optional): name:value; for re.sub() replacement functions
+    function (str; required): function body as (possibly multiline) string, with proper indentation
+
+    Returns:
+    str (specific to this project; could be told to return other types)
+    """
+    def __init__(self, name: str, constants: Optional[dict] = None, callbacks: Optional[dict] = None,
+                 function: str = None):
         self.__name__ = name
         self.constants = constants
         self.callbacks = callbacks
         self.function = function
 
-    def __call__(self, line):
+    def __call__(self, line: str) -> str:
         exec(self.function)
+
+
+# sample function; name is 'tuber', uses two constants, one of which is a regex
+a = F('tuber',
+      constants={
+          "pattern": re.compile(r"[AEIOU]"),
+          "age": 33},
+      function="""
+print(self.constants["pattern"].sub('x', line.upper()) + str(self.constants["age"]))
+""")
+a('spud')
